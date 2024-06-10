714d1280b5211fa4b257edffdc929b1c996e00a7
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-06-10 02:14:11 -0700

    Update bluefox.go

--- src/bluefox.go
+++ src/bluefox.go
@@ -17,7 +17,7 @@ func handleError(err error) int {
 	return 0
 }
 
-func spawnShell(conn net.Conn) {
+func spawnShell(conn net.Conn, shell string) {
 	addrInfo := strings.Split(conn.RemoteAddr().String(), ":")
 	ip := addrInfo[0]
 
@@ -76,13 +76,60 @@ func spawnShell(conn net.Conn) {
 			fmt.Printf("🔴 [ERROR] Could not update directory: %v\n", err)
 		}
 
-		cmd := exec.Command("/bin/bash", "-c", command)
+		cmd := exec.Command(shell, "-c", command)
 		cmd.Dir = dir
 		cmd.Stdout = conn
 		cmd.Stderr = conn
 		if err := cmd.Run(); handleError(err) == 1 {
 			fmt.Fprintf(conn, "🔴 [ERROR] Unable to execute commands: %v\n", err)
 		}
+	}
+}
+
+func spawnComm(conn net.Conn) {
+	addrInfo := strings.Split(conn.RemoteAddr().String(), ":")
+	ip := addrInfo[0]
+	defer conn.Close()
+
+	fmt.Printf("🟢 [SUCCESS] Received connection from %v\n", ip)
+	conn.Write([]byte("🦊 Connection established!\n"))
+
+	for {
+		input := make([]byte, 1024)
+		n, err := conn.Read(input)
+		if handleError(err) == 1 {
+			fmt.Printf("🔴 [ERROR] Could not read input from client: %v\n", err)
+			return
+		}
+
+		msg := strings.TrimSpace(string(input[:n]))
+		fmt.Printf(msg + "\n")
+
+		if msg == "exit" {
+			conn.Write([]byte("👋 Bye!\n"))
+			fmt.Printf("🟢 [SUCCESS] Connection from %v successfully closed\n", ip)
+			return
+		}
+	}
+}
+
+func listenShell(PORT string, SHELL string) {
+	ln, err := net.Listen("tcp", ":"+PORT)
+	if handleError(err) == 1 {
+		fmt.Printf("🔴 [ERROR] Unable to listen on specified port: %v\n", err)
+		return
+	} else {
+		fmt.Printf("🟡 [IDLE] Listening on port %s\n", PORT)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if handleError(err) == 1 {
+			fmt.Printf("🔴 [ERROR] Unable to establish connection: %v\n", err)
+		} else {
+			fmt.Printf("🟢 [SUCCESS] Connection established\n")
+		}
+		go spawnShell(conn, SHELL)
 	}
 }
 
@@ -102,69 +149,77 @@ func listen(PORT string) {
 		} else {
 			fmt.Printf("🟢 [SUCCESS] Connection established\n")
 		}
-		go spawnShell(conn)
+		go spawnComm(conn)
 	}
 }
 
 func connect(IP string, PORT string) {
-    conn, err := net.Dial("tcp", IP+":"+PORT)
-    if handleError(err) == 1 {
-        fmt.Printf("🔴 [ERROR] Unable to connect to %v on port %v: %v\n", IP, PORT, err)
-        return
-    }
-    defer conn.Close()
+	conn, err := net.Dial("tcp", IP+":"+PORT)
+	if handleError(err) == 1 {
+		fmt.Printf("🔴 [ERROR] Unable to connect to %v on port %v: %v\n", IP, PORT, err)
+		return
+	}
+	defer conn.Close()
 
-    reader := bufio.NewReader(os.Stdin)
-    serverReader := bufio.NewReader(conn)
+	reader := bufio.NewReader(os.Stdin)
+	serverReader := bufio.NewReader(conn)
 
-    for {
-        prompt, err := serverReader.ReadString('$')
-        if handleError(err) == 1 {
-            fmt.Printf("🔴 [ERROR] Could not read server prompt: %v\n", err)
-            return
-        }
+	for {
+		prompt, err := serverReader.ReadString('$')
+		if handleError(err) == 1 {
+			fmt.Printf("🔴 [ERROR] Could not read server prompt: %v\n", err)
+			return
+		}
 
 		prompt += string(' ')
-        fmt.Print(prompt)
+		fmt.Print(prompt)
 
-        input, err := reader.ReadString('\n')
-        if handleError(err) == 1 {
-            fmt.Printf("🔴 [ERROR] Could not read input: %v\n", err)
-            continue
-        }
+		input, err := reader.ReadString('\n')
+		if handleError(err) == 1 {
+			fmt.Printf("🔴 [ERROR] Could not read input: %v\n", err)
+			continue
+		}
 
-        input = strings.TrimSpace(input)
-        if input == "exit" {
-            return
-        }
+		input = strings.TrimSpace(input)
+		if input == "exit" {
+			return
+		}
 
-        _, err = conn.Write([]byte(input + "\n"))
-        if handleError(err) == 1 {
-            fmt.Printf("🔴 [ERROR] Could not send command: %v\n", err)
-            continue
-        }
-    }
+		_, err = conn.Write([]byte(input + "\n"))
+		if handleError(err) == 1 {
+			fmt.Printf("🔴 [ERROR] Could not send command: %v\n", err)
+			continue
+		}
+	}
 }
 
 func help() {
-	fmt.Printf("Usage: ./bluefox [OPTION] [ARGUMENT]\n")
+	fmt.Printf("Usage: ./bluefox [OPTION1] [ARGUMENT1] ... [OPTIONn] [ARGUMENTn]\n")
 	fmt.Printf("\nOptions:\n")
 	fmt.Printf("	-h, Shows help menu for this command\n")
 	fmt.Printf("	-l, Sets up listener for a specified port\n")
+	fmt.Printf("		--shell, spawns a specified shell supporting the -c argument\n")
 	fmt.Printf("	-c, Connects to a server based on a specified address and port\n")
 	fmt.Printf("\nFormat:\n")
 	fmt.Printf("	./bluefox -h\n")
 	fmt.Printf("	./bluefox -l <PORT>\n")
+	fmt.Printf("	./bluefox -l <PORT> --shell <SHELL>\n")
 	fmt.Printf("	./bluefox -c <IP> <PORT>\n")
 	fmt.Printf("\nExamples:\n")
 	fmt.Printf("	./bluefox -l 1234\n")
+	fmt.Printf("	./bluefox -l 1234 --shell /usr/bin/python3\n")
 	fmt.Printf("	./bluefox -c 127.0.0.1 1234\n")
 }
 
 func main() {
 	if len(os.Args) > 1 {
 		if strings.Compare(os.Args[1], "-l") == 0 {
-			listen(os.Args[2])
+			if len(os.Args) > 3 && strings.Compare(os.Args[3], "--shell") == 0 {
+				shell := os.Args[4]
+				listenShell(os.Args[2], shell)
+			} else {
+				listen(os.Args[2])
+			}
 		} else if strings.Compare(os.Args[1], "-c") == 0 {
 			var ipAddr string
 			if strings.Compare(os.Args[2], "localhost") == 0 {
